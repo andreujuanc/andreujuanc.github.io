05ccb7e8ffc558f9c8dfb039c087fa8daf3f9f28
Juan Carlos Andreu <jcandreu@kaya-consulting.com>
2019-04-10 09:58:46 +0200

    ok

--- src/bakersnotebook/privacy.js
+++ src/bakersnotebook/privacy.js
@@ -1,2 +1,4 @@
-import content from './privacy.md';
-console.log(content);
+import htmlContent from './privacy.md';
+import '../css/index.css'
+const contentDiv = document.getElementById('content');
+contentDiv.innerHTML = htmlContent;
